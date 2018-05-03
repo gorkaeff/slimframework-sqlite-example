e19b51085159676757e500f86e86c65acf343ce5
Jusue <Gorka.Jusue@thomsonreuters.com>
2018-05-03 16:07:24 +0200

    Signing the user up

--- app/Controllers/Auth/AuthController.php
+++ app/Controllers/Auth/AuthController.php
@@ -3,6 +3,7 @@
 namespace App\Controllers\Auth;
 
 use App\Controllers\Controller;
+use App\Models\User;
 
 class AuthController extends Controller
 {
@@ -13,6 +14,12 @@ class AuthController extends Controller
 
 	public function postSignUp($request, $response)
 	{
+		$user = User::create([
+			'email' => $request->getParam('email'),
+			'name' => $request->getParam('name'),
+			'password' => password_hash($request->getParam('password'), PASSWORD_DEFAULT)
+		]);
 
+		return $response->withRedirect($this->router->pathFor('home'));
 	}
 }
